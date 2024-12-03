38767b31652ac32bbf279a241662d6496ad9a1f1
wjdqjawns <108828118+wjdqjawns@users.noreply.github.com>
2024-12-03 13:24:43 +0900

    Create READ.m

---
+++ MAIN/READ.m
@@ -0,0 +1,14 @@
+[Author]
+정범준 - DGIST B.S
+민준하 - DGIST B.S
+
+[Date] 2024.12.03
+
+[What] DGIST MECH Term Project
+
+[Bill of Materials]
+MCU : teensy 4.0
+IMU : MPU6050
+Rotary Encoder : EN25-ABZ-2565
+Encoder Motor : IG42GM 01 Type 1/17
+Motor Driver : DMC16
